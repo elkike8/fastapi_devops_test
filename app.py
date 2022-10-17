b3108200dc8031e3e0babe40d090394940e9dae2
Fer Gonzalez <fergonzalez16@gmail.com>
2022-10-17 13:26:12 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/", StaticFiles(directory="html", html=True), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run(app)
